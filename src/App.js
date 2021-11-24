38dfa8242ac2914a8c7bf331ebbfd5b3873dc5bb
ErezM4N <erezm4n@gmail.com>
2021-11-24 12:59:15 +0200

    fixing start proj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-      <h1>Simple</h1>
-      <p>I'm main branch</p>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
